44ec16aaf7c5b8da47b49619739d56903e7d3aa8
Zahra Al Bulushi <47219252+zahra98a@users.noreply.github.com>
2022-02-19 17:32:53 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# In-App Payments Server Quickstart
+
+This is an example server with a single endpoint of `/chargeForCookie` for processing payments using Square. The server expects a `POST` request with a payload of `application/json` following the format:
+```
+{
+  "nonce": "INSERT_YOUR_NONCE_HERE"
+}
+```
+
+*Please replace "INSERT_YOUR_NONCE_HERE" with your own nonce that was generated from either the Square Payment Form or the In-App Payments SDK.*
+
+## Instructions
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/square/in-app-payments-server-quickstart/tree/master)
+
+* Click the **Deploy to Heroku** button
+* Login or create a Heroku account
+* Enter a unique **Heroku app name**.
+* Go to the [Square Application Dashboard](https://developer.squareup.com/apps) and select your application.
+* Copy the **Personal Access Token** from the **Credentials** tab into the `ACCESS_TOKEN` field of the Heroku configuration page.
+* Specify the environment by setting **ENVIRONMENT**. Sandbox use - `SANDBOX`; Production use - `PRODUCTION`.
+* Click **Deploy app**
+* Copy `https://[Heroku app name].herokuapp.com/chargeForCookie` as your URL to POST to in your mobile application.
+
+Example curl request you can make to the server:
+```
+curl -X POST 'https://[Heroku app name].herokuapp.com/chargeForCookie' \
+  -H 'Content-Type: application/json' \
+  -d '{ "nonce": "INSERT_YOUR_NONCE_HERE" }'
+```
+
+> You can find the test nonce values in the guide [Sandbox Test Values](https://developer.squareup.com/docs/testing/test-values).
+
+## Start localhost server
+
+* Go to the [Square Application Dashboard](https://developer.squareup.com/apps) and select your application.
+* Copy the **Personal Access Token** from the **Credentials** tab into the `ACCESS_TOKEN` field of the Heroku configuration page.
+* Specify the environment by setting **ENVIRONMENT**. Sandbox use - `SANDBOX`; Production use - `PRODUCTION`.
+* Run the following command in the root folder of this project:
+
+    `PORT=8000 ACCESS_TOKEN={{Replace_with_personal_access_token}} ENVIRONMENT=SANDBOX node index.js`
---
+++ index.js
@@ -0,0 +1,174 @@
+const express = require('express');
+const app = express();
+const { Client, Environment } = require("square");
+const crypto = require('crypto');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Initialized the Square Api client:
+//   Set environment
+//   Set access token
+const defaultClient = new Client({
+  environment: process.env.ENVIRONMENT === "PRODUCTION" ? Environment.Production : Environment.Sandbox,
+  accessToken: process.env.ACCESS_TOKEN,
+});
+
+const { paymentsApi, ordersApi, locationsApi, customersApi } = defaultClient;
+
+app.post('/chargeForCookie', async (request, response) => {
+  const requestBody = request.body;
+  try {
+    const listLocationsResponse = await locationsApi.listLocations();
+    const locationId = listLocationsResponse.result.locations[0].id;
+    const createOrderRequest = getOrderRequest(locationId);
+    const createOrderResponse = await ordersApi.createOrder(createOrderRequest);
+
+    const createPaymentRequest = {
+      idempotencyKey: crypto.randomBytes(12).toString('hex'),
+      sourceId: requestBody.nonce,
+      amountMoney: {
+        ...createOrderResponse.result.order.totalMoney,
+      },
+      orderId: createOrderResponse.result.order.id,
+      autocomplete: true,
+    };
+    const createPaymentResponse = await paymentsApi.createPayment(createPaymentRequest);
+    console.log(createPaymentResponse.result.payment);
+
+    response.status(200).json(createPaymentResponse.result.payment);
+  } catch (e) {
+    console.log(
+      `[Error] Status:${e.statusCode}, Messages: ${JSON.stringify(e.errors, null, 2)}`);
+
+    sendErrorMessage(e.errors, response);
+  }
+});
+
+app.post('/chargeCustomerCard', async (request, response) => {
+  const requestBody = request.body;
+
+  try {
+    const listLocationsResponse = await locationsApi.listLocations();
+    const locationId = listLocationsResponse.result.locations[0].id;
+    const createOrderRequest = getOrderRequest(locationId);
+    const createOrderResponse = await ordersApi.createOrder(createOrderRequest);
+    const createPaymentRequest = {
+      idempotencyKey: crypto.randomBytes(12).toString('hex'),
+      customerId: requestBody.customer_id,
+      sourceId: requestBody.customer_card_id,
+      amountMoney: {
+        ...createOrderResponse.result.order.totalMoney,
+      },
+      orderId: createOrderResponse.result.order.id
+    };
+    const createPaymentResponse = await paymentsApi.createPayment(createPaymentRequest);
+    console.log(createPaymentResponse.result.payment);
+
+    response.status(200).json(createPaymentResponse.result.payment);
+  } catch (e) {
+    console.log(
+      `[Error] Status:${e.statusCode}, Messages: ${JSON.stringify(e.errors, null, 2)}`);
+
+    sendErrorMessage(e.errors, response);
+  }
+});
+
+app.post('/createCustomerCard', async (request, response) => {
+  const requestBody = request.body;
+  console.log(requestBody);
+  try {
+    const createCustomerCardRequestBody = {
+      cardNonce: requestBody.nonce
+    };
+    const customerCardResponse = await customersApi.createCustomerCard(requestBody.customer_id, createCustomerCardRequestBody);
+    console.log(customerCardResponse.result.card);
+
+    response.status(200).json(customerCardResponse.result.card);
+  } catch (e) {
+    console.log(
+      `[Error] Status:${e.statusCode}, Messages: ${JSON.stringify(e.errors, null, 2)}`);
+
+    sendErrorMessage(e.errors, response);
+  }
+});
+
+function getOrderRequest(locationId) {
+  return {
+    idempotencyKey: crypto.randomBytes(12).toString('hex'),
+    order: {
+      locationId: locationId,
+      lineItems: [
+        {
+          name: "Cookie 🍪",
+          quantity: "1",
+          basePriceMoney: {
+            amount: 100,
+            currency: "EUR"
+          }
+        }
+      ]
+    }
+  }
+}
+
+function sendErrorMessage(errors, response) {
+  switch (errors[0].code) {
+    case "UNAUTHORIZED":
+      response.status(401).send({
+          errorMessage: "Server Not Authorized. Please check your server permission."
+      });
+      break;
+    case "GENERIC_DECLINE":
+      response.status(400).send({
+          errorMessage: "Card declined. Please re-enter card information."
+      });
+      break;
+    case "CVV_FAILURE":
+      response.status(400).send({
+          errorMessage: "Invalid CVV. Please re-enter card information."
+      });
+      break;
+    case "ADDRESS_VERIFICATION_FAILURE":
+      response.status(400).send({
+          errorMessage: "Invalid Postal Code. Please re-enter card information."
+      });
+      break;
+    case "EXPIRATION_FAILURE":
+      response.status(400).send({
+          errorMessage: "Invalid expiration date. Please re-enter card information."
+      });
+      break;
+    case "INSUFFICIENT_FUNDS":
+      response.status(400).send({
+          errorMessage: "Insufficient funds; Please try re-entering card details."
+      });
+      break;
+    case "CARD_NOT_SUPPORTED":
+      response.status(400).send({
+          errorMessage: "	The card is not supported either in the geographic region or by the MCC; Please try re-entering card details."
+      });
+      break;
+    case "PAYMENT_LIMIT_EXCEEDED":
+      response.status(400).send({
+          errorMessage: "Processing limit for this merchant; Please try re-entering card details."
+      });
+      break;
+    case "TEMPORARY_ERROR":
+      response.status(500).send({
+          errorMessage: "Unknown temporary error; please try again;"
+      });
+      break;
+    default:
+      response.status(400).send({
+          errorMessage: "Payment error. Please contact support if issue persists."
+      });
+      break;
+  }
+}
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
